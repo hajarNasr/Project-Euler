0e3c6104d1e9c395231f2c04c4defe18fbaf68d4
hajar nasr <hajar-nasr@hotmail.com>
2020-01-03 17:40:44 +0200

    it now works for numbers like 12

--- Problem 3/largest_prime_factor.py
+++ Problem 3/largest_prime_factor.py
@@ -1,13 +1,18 @@
 import sympy              # pip install sympy
 def largest_prime_factor(num):
-    prime = 2             # first prime number
-    arr = [1]
-    while True:
-        if not num % prime:      # check if num is divisibe by the current prime number,
-           arr[-1] *= prime      #   if yes => multiply prime to the last item of the list
-           if arr[-1] == num:    #   to check if the multiplication of all prime items so far equals num
-               return prime      #      if yes => return prime
-        prime = sympy.nextprime(prime) # if num is not divisible by the current prime go to the next one
+    prime, n = 2, num             # first prime number and let n = num
+    value, remainder, mul = 1, 1, 1
+    while value or remainder:  # the loop should terminate when both value and reminder equal to 0
+        value, remainder = divmod(n, prime)  # divide n by prime and assign the result to value and reminder
+        if remainder == 0:      
+            mul *= prime
+            if mul == num:          # check if we reach the value of num
+                return prime        # if yes => return the largest prime
+            else: 
+                n = value           # else reassign n to the new value
+        else:  # if remainder does't equal zero, the current prime isn't a factor of num
+           prime = sympy.nextprime(prime)  # go find the next prime 
+
             
 
    
